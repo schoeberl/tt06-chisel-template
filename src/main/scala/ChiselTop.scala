c6b7c41dee728649870949b3d27bd3fb9b48cb88
Martin Schoeberl <martin@jopdesign.com>
2024-04-11 23:17:15 +0200

    cleanup

--- src/main/scala/ChiselTop.scala
+++ src/main/scala/ChiselTop.scala
@@ -13,16 +13,7 @@ class ChiselTop() extends Module {
     val uio_oe = Output(UInt(8.W))    // IOs: Enable path (active high: 0=input, 1=output)
     val ena = Input(Bool())           // will go high when the design is enabled
   })
-/*
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
- */
+
   io.uio_out := 0.U
   // use bi-directionals as input
   io.uio_oe := 0.U
